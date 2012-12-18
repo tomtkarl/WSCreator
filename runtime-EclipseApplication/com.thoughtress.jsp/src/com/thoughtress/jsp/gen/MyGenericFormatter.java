f061de3fcd18d3e705348d31223ebc0398c1f381
Karl <Karl@vafud>
2012-12-18 00:10:19 +0000

    Added `[protected]` code blocks to GenericMF gen, refs #24, #9.

--- runtime-EclipseApplication/com.thoughtress.jsp/src/com/thoughtress/jsp/gen/MyGenericFormatter.java
+++ runtime-EclipseApplication/com.thoughtress.jsp/src/com/thoughtress/jsp/gen/MyGenericFormatter.java
@@ -2,15 +2,22 @@ package com.thoughtress.jsp.gen;
 
 public class MyGenericFormatter extends MessageFormatter{
 	public static String[] getTypes(){
+	//Start of user code method
 		throw new UnsupportedOperationException("Not yet implemented");
+	//End of user code
 	}
 	@Override
 	public String parseToFormat(Response answer) {
+	//Start of user code method
 		throw new UnsupportedOperationException("Not yet implemented");
+	//End of user code
 	}
+	
 	@Override
 	public Request parseToRequest(String data) {
+	//Start of user code method
 		throw new UnsupportedOperationException("Not yet implemented");
+	//End of user code
 	}
 	
 }
