56ff2d91582cce7a874227839b0a4a109d906835
Karl <Karl@vafud>
2013-02-26 15:46:56 +0000

    Add `TwitterFunctionProvider` to demo app model, and generate. refs #42

---
+++ runtime-EclipseApplication/com.thoughtress.jsp/src/com/thoughtress/jsp/gen/TwitterFunctionProvider.java
@@ -0,0 +1,29 @@
+package com.thoughtress.jsp.gen;
+//Start of user code imports
+//End of user code
+/**
+*A FunctionProvider for the Web Service<br />
+*Implements one or more functions to be made available through the service.
+*/
+public class TwitterFunctionProvider extends FunctionProvider {
+	//Start of user code classvars
+	//End of user code
+	/**
+	*Match the functions defined by this FunctionProvider to
+	*the method of the given MessagePart.
+	*
+	*@param  req  The MessagePart object to be matched against.
+	*@return      A Boolean value denoting whether this FunctionProvider matches the given MessagePart.
+	*/
+	public static Boolean match(MessagePart req) {
+		//Start of user code match
+        throw new UnsupportedOperationException("Not yet implemented");
+        //End of user code
+	}
+
+	public MessagePart process(MessagePart req) {
+		//Start of user code process
+        throw new UnsupportedOperationException("Not yet implemented");
+        //End of user code
+	}
+}
